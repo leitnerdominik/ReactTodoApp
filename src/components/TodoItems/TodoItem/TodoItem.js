2b044ce29207366e6816e3ca55e7ef09d29bbe1c
LeitnerBua <leitner.dominik@hotmail.de>
2018-06-07 23:17:26 +0200

    recreated todoitem component

--- src/components/TodoItems/TodoItem/TodoItem.js
+++ src/components/TodoItems/TodoItem/TodoItem.js
@@ -8,31 +8,13 @@ import classes from './TodoItem.css'
 const todoItem = (props) => {  
   console.log(props);
   return (
-    <div className={classes.Container}>
-      <label 
-          className={props.completed ? classes.Completed : null}
-          
-          
-          >{props.val}
-      </label>
-      <span className={classes.Checkmark}
-      onClick={props.markCompleted}
-      htmlFor={"chkbx" + props.val}></span>
-      <input type="checkbox" id={"chkbx" + props.val} />
- 
-      </div>
-      
-    // <li className={classes.Container}>
-    //   <input type="checkbox" className={classes.Hiddenchkbx} id={props.key}/>
-    //   <label className={props.completed ? [classes.ChkLabel, classes.Done].join(" ") : classes.chklbl}
-    //     onClick={props.markCompleted}
-    //     htmlFor={props.key}>
-      
-    //   <span className={classes.SpanBox}></span>
-    //   {props.val}
-    //   </label>
-    //   <button className={classes.DeleteItem} onClick={props.remove}>x</button>
-    // </li>
+    <li 
+    className={props.completed ? [classes.Container, classes.Completed].join(" ") : classes.Container}
+      onClick={props.markCompleted}>
+      {props.val}
+
+    <span className={classes.Close}>X</span>
+    </li>
     
   );
 }

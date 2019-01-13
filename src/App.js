92bcbecf37f05141afe57f1b0702d4eb42e6f8f3
Dominik Leitner <leitner.dominik@hotmail.de>
2019-01-13 21:44:32 +0100

    removed plus icon

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component, Fragment } from "react";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { far } from "@fortawesome/free-regular-svg-icons";
 import {
   faCircle,
   faPencilAlt,
   faTrashAlt,
-  faPlus,
   faCheckCircle
 } from "@fortawesome/free-solid-svg-icons";
 
@@ -15,7 +13,7 @@ import Todo from "./container/Todo/Todo";
 
 import "./App.css";
 
-library.add(far, faCircle, faPencilAlt, faTrashAlt, faPlus, faCheckCircle);
+library.add(far, faCircle, faPencilAlt, faTrashAlt, faCheckCircle);
 
 class App extends Component {
   render() {
